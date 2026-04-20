2e6506a26616d92e3519455c24768a8d0d2fa568
BetoFigu66 <betofigu@gmail.com>
2026-04-20 10:52:05 -0300

    Removido import não utilizado.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from 'react'
+import { useState, useEffect } from 'react'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import PhonePanel from './components/PhonePanel'
