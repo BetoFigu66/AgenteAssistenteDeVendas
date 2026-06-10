431e59d95845d13c55c37e220a7ff08b2c4e56e7
BetoFigu66 <betofigu@gmail.com>
2026-06-10 17:00:22 -0300

    Ajustes para rodar ambiente de dev + qa.

--- backend/main.py
+++ backend/main.py
@@ -985,4 +985,9 @@ async def health_check():
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run(
+        "main:app",
+        host=settings.API_HOST,
+        port=settings.API_PORT,
+        reload=True,
+    )
