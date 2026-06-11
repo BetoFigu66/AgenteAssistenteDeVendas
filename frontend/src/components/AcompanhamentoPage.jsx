64123184598aa4f01e4512b27dddd14877b2db5f
BetoFigu66 <betofigu@gmail.com>
2026-06-11 08:34:45 -0300

    BUG em CTF-001-03 — Mostrar a data/hora correta

--- frontend/src/components/AcompanhamentoPage.jsx
+++ frontend/src/components/AcompanhamentoPage.jsx
@@ -295,6 +295,7 @@ function AcompanhamentoPage() {
     return date.toLocaleString('pt-BR', {
       day: '2-digit',
       month: '2-digit',
+      year: '2-digit',
       hour: '2-digit',
       minute: '2-digit'
     })
